81fcfd621d00c341ea41bbd2a394322691a67140
soheila91 <116343038+soheila91@users.noreply.github.com>
2025-07-09 22:55:34 +0330

    Add files via upload

---
+++ pic1.py
@@ -0,0 +1,11 @@
+import turtle
+t= turtle.Turtle()
+t.speed(-1)
+length=10
+step=10
+
+for i in range(20):
+    t.fd(length)
+    t.left(90)
+    length +=step
+    
---
+++ pic3- 1.py
@@ -0,0 +1,16 @@
+import turtle
+t= turtle.Turtle()
+t.speed(0)
+length=10
+step=10
+
+for i in range(20):
+    for i in range (4):
+        t.fd(length)
+        t.left(90)
+  
+    length +=step
+    
+    
+
+    
